f61d62d05391dcfa1c0733f3915c621ab71c810a
dennis wu <dennis wu>
2017-04-07 00:57:09 +0800

    update

---
+++ index.js
@@ -0,0 +1,15 @@
+'use strict';
+
+const Koa = require('koa');
+const app = new Koa();
+const mongoose = require('mongoose');
+
+mongoose.connect(process.env.PROD_MONGODB)
+
+app.use(ctx => {
+    ctx.body = 'hello, wolrd'
+});
+
+app.listen(process.env.PORT || 5000);
+
+console.log('server started. you can visit it at http://localhost')
